238e4d7b6c18d04bb223ccfd5aea82016bfb12a9
JanuLakshmanan <azhagujanu@gmail.com>
2020-03-06 15:19:49 +0530

    2 files modified

--- index.js
+++ index.js
@@ -1,6 +1,31 @@
-// Import stylesheets
-import './style.css';
+const add=document.getElementById("increment");
+const sub=document.getElementById("decrement");
+inputbox=document.getElementById("in");
+values=0;
+function addValue(){
+   inputvalue= document.getElementById("in").value;
+   if(parseInt(inputvalue)<10 )
+   {
+       values=parseInt(inputvalue)+1;
+   inputbox.value=values;
+   document.getElementById("increment").disabled = false;
+  }
+  else{
+    document.getElementById("increment").disabled = true;
+  }
+}
+function minusValue(){
+    if(parseInt(inputvalue)<=10 && parseInt(inputvalue)>0 )
+    {inputvalue= document.getElementById("in").value;
+    values=parseInt(inputvalue)-1;
+    inputbox.value=values;
+    document.getElementById("increment").disabled = false;}
+    else
+    {
+        document.getElementById("decrement").disabled = true;
+    }
+}
 
-// Write Javascript code!
-const appDiv = document.getElementById('app');
-appDiv.innerHTML = `<h1>JS Starter</h1>`;
+inputbox.value=values;
+add.addEventListener("click",addValue);
+sub.addEventListener("click",minusValue);
